f3dfdf003e8505cbe9b591a843871d9a450c0e8a
Ieuan Horton <HortonIeuan@gmail.com>
2020-08-17 16:33:21 -0400

    Start

---
+++ StoredProcedureQualityOfLife/CreateInputParameter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoredProcedureQualityOfLife
+{
+
+    /*
+     *  Ieuan Horton
+     *  CreateInputParameter assists in the creation of a SqlParameter
+     */
+
+    public class CreateInputParameter
+    {
+        public SqlParameter CreateVarChar(SqlParameter sqlp)
+        {
+            sqlp.Direction = ParameterDirection.Input;
+            sqlp.SqlDbType = SqlDbType.VarChar;
+            sqlp.Size = 50;
+            return sqlp;
+        }
+
+        public SqlParameter CreateVarChar(SqlParameter sqlp, bool max)
+        {
+            sqlp.Direction = ParameterDirection.Input;
+            sqlp.SqlDbType = SqlDbType.VarChar;
+            if (max == false)
+            {
+                sqlp.Size = 50;
+            }
+            return sqlp;
+        }
+        public SqlParameter CreateDateTime(SqlParameter sqlp)
+        {
+            sqlp.Direction = ParameterDirection.Input;
+            sqlp.SqlDbType = SqlDbType.DateTime;
+            return sqlp;
+        }
+
+        public SqlParameter CreateVarBinary(SqlParameter sqlp, bool max)
+        {
+            sqlp.Direction = ParameterDirection.Input;
+            sqlp.SqlDbType = SqlDbType.VarBinary;
+            if (max == false)
+            {
+                sqlp.Size = 50;
+            }
+            return sqlp;
+        }
+        public SqlParameter CreateVarChar(SqlParameter sqlp, int size)
+        {
+            sqlp.Direction = ParameterDirection.Input;
+            sqlp.SqlDbType = SqlDbType.VarChar;
+            sqlp.Size = size;
+            return sqlp;
+        }
+
+        public SqlParameter CreateInt(SqlParameter sqlp)
+        {
+            sqlp.Direction = ParameterDirection.Input;
+            sqlp.SqlDbType = SqlDbType.Int;
+            sqlp.Size = 4;
+            return sqlp;
+        }
+
+        public SqlParameter CreateInt(SqlParameter sqlp, int size)
+        {
+            sqlp.Direction = ParameterDirection.Input;
+            sqlp.SqlDbType = SqlDbType.Int;
+            sqlp.Size = size;
+            return sqlp;
+        }
+    }
+}
---
+++ StoredProcedureQualityOfLife/FillParameters.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoredProcedureQualityOfLife
+{
+    /*
+     *  Ieuan Horton
+     *  FillParameters creates an inputParameter and adds it to the objCommand
+     */
+
+    public class FillParameters
+    {
+        CreateInputParameter cip = new CreateInputParameter();
+
+        //Adds a string parameter to the objCommand object
+        public void AddParameter(ref SqlCommand objCommand, string pName, string pValue)
+        {
+            SqlParameter ip = new SqlParameter(pName, pValue);
+            ip = cip.CreateVarChar(ip);
+            objCommand.Parameters.Add(ip);
+        }
+
+        //Add a date time
+        public void AddParameter(ref SqlCommand objCommand, string pName, DateTime pValue)
+        {
+            SqlParameter ip = new SqlParameter(pName, pValue);
+            ip = cip.CreateDateTime(ip);
+            objCommand.Parameters.Add(ip);
+        }
+
+        //Adds a string parameter to the objCommand object with max size
+        public void AddParameter(ref SqlCommand objCommand, string pName, string pValue, bool max)
+        {
+            SqlParameter ip = new SqlParameter(pName, pValue);
+            ip = cip.CreateVarChar(ip, true);
+            objCommand.Parameters.Add(ip);
+        }
+
+        //Adds a binary parameter to the objCommand object with max size
+        public void AddParameter(ref SqlCommand objCommand, string pName, byte[] pValue, bool max)
+        {
+            SqlParameter ip = new SqlParameter(pName, pValue);
+            ip = cip.CreateVarBinary(ip, true);
+            objCommand.Parameters.Add(ip);
+        }
+
+
+        //Adds a string parameter to the objCommand object at size
+        public void AddParameter(ref SqlCommand objCommand, string pName, string pValue, int size)
+        {
+            SqlParameter ip = new SqlParameter(pName, pValue);
+            ip = cip.CreateVarChar(ip, size);
+            objCommand.Parameters.Add(ip);
+        }
+
+        //Adds an int parameter to the objcommand object
+        public void AddParameter(ref SqlCommand objCommand, string pName, int pValue)
+        {
+            SqlParameter ip = new SqlParameter(pName, pValue);
+            ip = cip.CreateInt(ip);
+            objCommand.Parameters.Add(ip);
+        }
+
+        //Adds a string parameter of bytes to the objCommand object. This is used to store serialized objects
+        public void AddParameter(ref SqlCommand objCommand, string pName, byte[] pValue)
+        {
+            SqlParameter ip = new SqlParameter(pName, pValue);
+            ip = cip.CreateVarChar(ip);
+            objCommand.Parameters.Add(ip);
+        }
+    }
+}
